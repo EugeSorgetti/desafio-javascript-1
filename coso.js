5a401aa7e38d380ed402196596cfe823a520842d
EugeSorgetti <eugesorgetti@gmail.com>
2021-05-23 21:30:43 -0300

    Carrusel (casi) responsive

--- coso.js
+++ coso.js
@@ -40,13 +40,33 @@ carouselSlide.style.transform = 'translate(' + (-size * counter) + 'px)';
 //Button listener
 
 next.addEventListener('click', ()=> {
+    if(counter >= carouselImages.length -1) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
 });
 
 prev.addEventListener('click', ()=> {
+    if (counter <= 0) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', ()=> {
+    if (carouselImages[counter].id === "last-clone") {
+        carouselSlide.style.transition = "none";
+        console.log("none");
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
+    }
+});
+
+carouselSlide.addEventListener('transitionend', ()=> {
+    if (carouselImages[counter].id === "first-clone") {
+        carouselSlide.style.transition = "none";
+        console.log("none");
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
+    }
 });
